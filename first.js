ca707fcbb1954e7e27c14efd47f002ecab0f31be
Monil Gangar <gmonil@users.noreply.github.com>
2019-10-15 19:05:44 +0530

    cleaned up

--- first.js
+++ first.js
@@ -1,22 +1,3 @@
 class First {
-    foo() {
-        console.log("This is foo");
-    }
-	bar()
-	{
-		console.log("this is bar");
-	}
-	
-	bar2()
-	{
-		console.log("I am in bar");
-    }
     
-    foo2() {
-        console.log('This is foo2');
-    }
-	bar3()
-	{
-		console.log("I am in bar 3");
-	}	
 }
